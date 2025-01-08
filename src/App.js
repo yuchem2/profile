b06e94ae474e801ef49fd0d62ab67605567c70eb
Yoon Jaehyun <yuchem2@gmail.com>
2025-01-08 21:01:48 +0900

    feat: add layout for profile

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css'
-
-import { PUBLIC_URL } from './config'
+import Content from './component/content'
 
 export default function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={`${PUBLIC_URL}/logo512.png`} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div className="Profile">
+            <div className="Profile-container">
+                <div className="Profile-block">
+                    <span className="Profile-header">ABOUT ME</span>
+                    <span className="Profile-paragraph">(짧은 자기소개)</span>
+                </div>
+                <div className="Profile-block">
+                    <span className="Profile-header">CONTACT</span>
+                </div>
+            </div>
+            <Content title="EDUCATION"></Content>
+            <Content title="SKILLS"></Content>
+            <Content title="CAREER"></Content>
+            <Content title="AWARDS"></Content>
+            <Content title="PROJECT"></Content>
+            <Content title="OTHERS"></Content>
         </div>
     )
 }
