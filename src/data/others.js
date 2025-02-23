7432d2d0b3f7bf952363239612b53610fc4e692f
Jaehyun Yoon <yuchem2@gmail.com>
2025-02-23 15:15:40 +0900

    feat: add others (#6)

---
+++ src/data/others.js
@@ -0,0 +1,17 @@
+export const others = [
+    {
+        title: 'Beyond-Imagination 동아리활동',
+        duration: '2023.10-ing',
+        description: '동아리 활동',
+        paragraph:
+            '학과 졸업생들이 모여 만든 IT 개발 스터디입니다. 동아리에서는 상시 프로젝트 수행, 공모전 참여, 개발 스터디, 멘토링 등의 활동을 진행하고 있습니다.' +
+            '현직에서 활동하는 개발자들과 함께 다양한 프로젝트를 경험하면서 협업과 기술을 배우고 있습니다.',
+    },
+    {
+        title: 'Programming Language Center 근무',
+        duration: '2023.03-2024.12',
+        description: '고려대학교 세종캠퍼스\n컴퓨터융합소프트웨어학과',
+        paragraph:
+            '학과에서 운영하는 프로그래밍 언어 학습 도움 센터로, 학기 시작 전 코딩테스트 및 면접을 보고 선발된 학생들이 근무하는 곳입니다. 주 활동은 프로그래밍 수업(C, C++, Python, Java 등)을 듣는 학생들의 질문에 답변하는 것입니다.',
+    },
+]
